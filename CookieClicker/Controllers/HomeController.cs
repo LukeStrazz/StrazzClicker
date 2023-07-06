f4eb4046e78ff219f6d7544d2c626a5cd3224453
Luke-Angelo Strazzera <98197481+LukeStrazz@users.noreply.github.com>
2023-07-05 20:33:06 -0400

    imma game dev

---
+++ CookieClicker/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using CookieClicker.Models;
+
+namespace CookieClicker.Controllers;
+
+public class HomeController : Controller
+{
+    private const string CounterKey = "Counter";
+
+    public IActionResult Index()
+    {
+        // Initialize the counter if it doesn't exist
+        if (HttpContext.Session.GetInt32(CounterKey) == null)
+        {
+            HttpContext.Session.SetInt32(CounterKey, 0);
+        }
+
+        // Pass the counter to the view
+        ViewBag.Counter = HttpContext.Session.GetInt32(CounterKey);
+
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult IncrementCounter()
+    {
+        int counter = HttpContext.Session.GetInt32(CounterKey) ?? 0;
+        counter++;
+
+        HttpContext.Session.SetInt32(CounterKey, counter);
+
+        return Json(new { status = "success", counter = counter });
+    }
+}
